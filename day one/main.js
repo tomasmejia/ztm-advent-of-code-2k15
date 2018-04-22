f8c0b9e76008b883876c2216ae1d75304c6f5091
Cloudburst <tomas.mejia.ed@hotmail.com>
2018-04-21 22:41:07 -0300

    Final commit

--- day one/main.js
+++ day one/main.js
@@ -1,23 +1,32 @@
+// toDo, find a way to do it with functional programming methods
+// like map, reduce, filter, some, every... 
 const fs = require('fs');
-
-// Santa starts at floor 0
-// ( Go up 1 floor
-// ) Go down 1 floor
-//  (()) and()() both result in floor 0.
-//  (((and(()(()(both result in floor 3.
-//  ))(((((also results in floor 3.
-//  ()) and))(both result in floor - 1(the first basement level).
-//  ))) and ) ()) ()) both result in floor - 3.
-
-// To what floor do the instructions take Santa ?
 
 fs.readFile("./finalFloor.txt", (err, content) => {
     console.time("time");
-        if (err) throw err;
-    const floors = content.toString().split('');
-    const filteredUp = floors.filter(val => val.includes("(")).length;
-    const filteredDown = floors.filter(val => val.includes(")")).length;
-    console.log(filteredUp - filteredDown);
+    if (err) throw err;
+    // part one
+    const floors = content.toString()
+    const splitFloors = floors.split('');
+    const filteredUp = splitFloors.filter(val => val.includes("(")).length;
+    const filteredDown = splitFloors.filter(val => val.includes(")")).length;
+    console.log("Santa needs to go to the floor", filteredUp - filteredDown);
+
+    //part two
+    let pos = 0;
+    for (let i = 0; i < floors.length; i++) {
+        if (floors.charAt(i) === ')') {
+            pos--
+        } else {
+            pos++
+        }
+        if (pos < 0) {
+            console.log("Santa enters the basement at position:", i + 1)
+            break;
+        }
+    }
+
+
     console.timeEnd("time");
 });
 
